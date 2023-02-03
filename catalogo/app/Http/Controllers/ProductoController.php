6efa735d620ce62618d76adfab1056750ed99fa7
Marcos <exegeses@gmail.com>
2023-02-03 13:02:56 -0300

    método para validar formulario

--- catalogo/app/Http/Controllers/ProductoController.php
+++ catalogo/app/Http/Controllers/ProductoController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Categoria;
+use App\Models\Marca;
 use App\Models\Producto;
 use Illuminate\Http\Request;
 
@@ -27,7 +29,43 @@ class ProductoController extends Controller
      */
     public function create()
     {
-        //
+        //obtenemos listados de marcas y categorias
+        $marcas = Marca::all();
+        $categorias = Categoria::all();
+        return view('productoCreate',
+            [
+                'marcas'=>$marcas,
+                'categorias'=>$categorias
+            ]
+        );
+    }
+
+    private function validarForm( Request $request )
+    {
+        $request->validate(
+            [
+                'prdNombre'=>'required|unique:productos,prdNombre|min:3|max:75',
+                'prdPrecio'=>'required|numeric|min:0',
+                'idMarca'=>'required',
+                'idCategoria'=>'required',
+                'prdDescripcion'=>'max:255',
+                'prdImagen'=>'mimes:jpg,jpeg,png,gif,webp,svg|max:2048'
+            ],
+            [
+                'prdNombre.required'=>'El campo "Nombre del producto" es obligatorio.',
+                'prdNombre.unique'=>'El "Nombre del producto" ya existe.',
+                'prdNombre.min'=>'El campo "Nombre del producto" debe tener como mínimo 3 caractéres.',
+                'prdNombre.max'=>'El campo "Nombre" debe tener 75 caractéres como máximo.',
+                'prdPrecio.required'=>'Complete el campo Precio.',
+                'prdPrecio.numeric'=>'Complete el campo Precio con un número.',
+                'prdPrecio.min'=>'Complete el campo Precio con un número mayor a 0.',
+                'idMarca.required'=>'Seleccione una marca.',
+                'idCategoria.required'=>'Seleccione una categoría.',
+                'prdDescripcion.max'=>'Complete el campo Descripción con 255 caractéres como máxino.',
+                'prdImagen.mimes'=>'Debe ser una imagen.',
+                'prdImagen.max'=>'Debe ser una imagen de 2MB como máximo.'
+            ]
+        );
     }
 
     /**
@@ -38,7 +76,8 @@ class ProductoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //validación
+        return 'pasó validación';
     }
 
     /**
